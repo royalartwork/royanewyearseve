f334e979c5d0bdc2daf9408b5d6eed4f6a616142
royalartwork <royalartwork8@gmail.com>
2024-12-19 10:16:21 +0700

    Add files via upload

---
+++ appConfig.js
@@ -0,0 +1,10 @@
+module.exports = {
+  title: "JUDUL",
+  tagline: "",
+  description: "",
+  keywords: "",
+  author: "Milyader Muda",
+  favicon: "",
+  imageCount: 20,
+  removeBadWords: "porn,nude,sex",
+};
