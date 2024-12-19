609facf15b4e17679480431a1a35982b68ffd165
royalartwork <royalartwork8@gmail.com>
2024-12-19 10:17:51 +0700

    Update appConfig.js

--- appConfig.js
+++ appConfig.js
@@ -1,10 +1,12 @@
 module.exports = {
-  title: "JUDUL",
-  tagline: "",
-  description: "",
-  keywords: "",
+  title: "New Year's Eve",
+  tagline: "Capturing the magic of New Year moments. Find inspiration for unforgettable celebrations and traditions",
+  description: "Capturing the magic of New Year moments. Find inspiration for unforgettable celebrations and traditions",
+  keywords: "New Year's Eve,New year's eve outfit,New Year's Eve imdb,New year eve dti,New Year celebration",
   author: "Milyader Muda",
   favicon: "",
   imageCount: 20,
   removeBadWords: "porn,nude,sex",
 };
+
+
